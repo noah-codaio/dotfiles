1135aa5bc46281e9bb663a72185cc5437c3c3539
Noah Silverstein <47999491+noah-codaio@users.noreply.github.com>
2024-11-09 20:15:44 -0500

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -29,9 +29,6 @@ brew bundle --file ./Brewfile
 # Create a projects directories
 mkdir $HOME/Code
 
-# Clone Github repositories
-./clone.sh
-
 # Symlink the Mackup config file to the home directory
 ln -s ./.mackup.cfg $HOME/.mackup.cfg
 
